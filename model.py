245c56b64c974d8c762839931579735303175b39
FUFSoB <fufsob@fufsob.ru>
2024-05-14 22:33:26 +0500

    real-time prediction

--- model.py
+++ model.py
@@ -6,7 +6,8 @@ import keras
 import json
 from PIL import Image
 from io import BytesIO
-import os
+import time
+import threading
 
 from utils import resize_image, pretty_predictions
 
@@ -40,6 +41,15 @@ def draw_kana():
     root.title("Draw Kana")
     root.resizable(True, True)
 
+    buttons_frame = tk.Frame(root)
+
+    def threaded(fn):
+        def wrapper(*args, **kwargs):
+            t = threading.Thread(target=fn, args=args, kwargs=kwargs)
+            t.start()
+
+        return wrapper
+
     def paint(event):
         # print("paint")
         x1, y1 = (event.x - 1), (event.y - 1)
@@ -57,10 +67,9 @@ def draw_kana():
         image[image < 245] = 245
         bytes_image = BytesIO()
         image = Image.fromarray(image)
-        # change black to gray
         image.save(bytes_image, format="PNG")
         canvas.image = None
-        canvas.image = tk.PhotoImage("data", data=bytes_image.getvalue())
+        canvas.image = tk.PhotoImage("hint", data=bytes_image.getvalue())
         canvas.create_image(0, 0, image=canvas.image, anchor="nw")
         entry.delete(0, "end")
         entry.insert(0, item["kana"])
@@ -72,17 +81,14 @@ def draw_kana():
     def predict():
         # print("predict")
         ps = canvas.postscript(colormode="color")
-        with open("temp.ps", "w") as f:
-            f.write(ps)
-        img = Image.open("temp.ps")
+        img = Image.open(BytesIO(bytes(ps,'ascii')))
         img = np.array(img)
-        os.remove("temp.ps")
         prediction, new_image = predict_image(img)
         new_image = Image.fromarray(new_image)
         b = BytesIO()
         new_image.save(b, format="PNG")
         predict_canvas.image = None
-        predict_canvas.image = tk.PhotoImage("data", data=b.getvalue())
+        predict_canvas.image = tk.PhotoImage("prediction", data=b.getvalue())
         predict_canvas.create_image(0, 0, image=predict_canvas.image, anchor="nw")
         text = pretty_predictions(prediction, entry.get() or None)
         guess_label.config(text=text)
@@ -92,33 +98,41 @@ def draw_kana():
         # print("close")
         root.destroy()
 
+    real_time = tk.BooleanVar()
+
     canvas = tk.Canvas(root, width=200, height=200, bg="white")
     canvas.bind("<B1-Motion>", paint)
+    canvas.bind("<ButtonRelease-1>", lambda e: real_time.get() and threaded(predict)())
     canvas.pack()
 
     # input box for kana from dataset
     should_be_input = tk.StringVar()
-    entry = tk.Entry(root, textvariable=should_be_input)
+    entry = tk.Entry(buttons_frame, textvariable=should_be_input)
     entry.pack()
 
-    button = tk.Button(root, text="Predict", command=predict)
+    button = tk.Button(buttons_frame, text="Predict", command=threaded(predict))
     button.pack()
 
-    button = tk.Button(root, text="Clear", command=clear)
+    # checkbox "Real-time"
+    checkbox = tk.Checkbutton(buttons_frame, text="Real-time", variable=real_time)
+    checkbox.pack()
+
+    button = tk.Button(buttons_frame, text="Clear", command=clear)
     button.pack()
 
-    button = tk.Button(root, text="Set Hint", command=hint)
+    button = tk.Button(buttons_frame, text="Set Hint", command=hint)
     button.pack()
 
-    button = tk.Button(root, text="Close", command=close)
+    button = tk.Button(buttons_frame, text="Close", command=close)
     button.pack()
 
-    predict_canvas = tk.Canvas(root, width=64, height=64, bg="white")
+    predict_canvas = tk.Canvas(buttons_frame, width=64, height=64, bg="white")
     predict_canvas.pack()
 
-    guess_label = tk.Label(root, text="Draw a kana")
+    guess_label = tk.Label(buttons_frame, text="Draw a kana")
     guess_label.pack()
 
+    buttons_frame.pack()
     root.mainloop()
 
 
